aa2a045aaa4fed4ef87514c6dca9556a619d03aa
Adracus <adracus@gmail.com>
2015-01-08 21:42:14 +0100

    Additional description for command line parts
    add additional description for command line parts
    (inspired by git)
    
    fixes #13

--- bin/dart_coveralls.dart
+++ bin/dart_coveralls.dart
@@ -5,8 +5,11 @@ import "src/calculate_and_send_coverage.dart";
 void main(List<String> args) {
   var builder = new CommandLineHubBuilder();
   builder
-    ..addPart("report", new ReportPart())
-    ..addPart("calc", new CalcPart());
+    ..addPart("report", new ReportPart(),
+        description: "Calculate and report coverage data to coveralls")
+    ..addPart("calc", new CalcPart(),
+        description: "Calculate coveralls data and output it or store " +
+        "it in a file");
   var hub = builder.build();
 
   return hub.parseAndExecute(args);
--- bin/src/command_line.dart
+++ bin/src/command_line.dart
@@ -1,5 +1,7 @@
 library cmdpart;
 
+import "dart:math" show max;
+
 import "package:args/args.dart";
 
 export "package:args/args.dart";
@@ -13,10 +15,10 @@ abstract class CommandLinePart {
 }
 
 class CommandLineHubBuilder {
-  final Map<String, CommandLinePart> _parts = {};
+  final Map<PartInfo, CommandLinePart> _parts = {};
 
-  void addPart(String name, CommandLinePart part) {
-    _parts[name] = part;
+  void addPart(String name, CommandLinePart part, {String description: ""}) {
+    _parts[new PartInfo(name, description: description)] = part;
   }
 
   CommandLinePart removePart(String name) => _parts.remove(name);
@@ -25,28 +27,63 @@ class CommandLineHubBuilder {
 }
 
 class CommandLineHub extends Object with CommandLinePart {
-  final Map<String, CommandLinePart> _parts;
+  final Map<PartInfo, CommandLinePart> _parts;
   final ArgParser parser;
-  final String _usage;
 
-  CommandLineHub._(Map<String, CommandLinePart> parts)
+  CommandLineHub._(Map<PartInfo, CommandLinePart> parts)
       : _parts = parts,
-        parser = _initializeParser(parts),
-        _usage = "Possible commands are: ${parts.keys.join(", ")}";
+        parser = _initializeParser(parts);
 
   void execute(ArgResults results) {
-    if (results["help"]) return print(_usage);
+    if (results["help"]) return print(usage);
     if (null == results.command) {
-      return print(_usage);
+      return print(usage);
     }
-    var part = _parts[results.command.name];
+    var part = partByName(results.command.name);
     part.execute(results.command);
   }
+  
+  CommandLinePart partByName(String name) {
+    var partInfo = _parts.keys.firstWhere((info) => info.name == name);
+    return _parts[partInfo];
+  }
+  
+  int _getLongestNameLength() {
+    var longest = 0;
+    _parts.keys.forEach((part) => longest = max(longest, part.name.length));
+    return longest;
+  }
+  
+  String get usage {
+    int len = _getLongestNameLength();
+    return "Possible commands are: \n\n" +
+        _parts.keys.map((info) => info.toString(len)).join("\n");
+  }
 
-  static ArgParser _initializeParser(Map<String, CommandLinePart> parts) {
+  static ArgParser _initializeParser(Map<PartInfo, CommandLinePart> parts) {
     var parser = new ArgParser(allowTrailingOptions: false);
-    parts.forEach((name, part) => parser.addCommand(name, part.parser));
+    parts.forEach((info, part) => parser.addCommand(info.name, part.parser));
     parser.addFlag("help", negatable: false);
     return parser;
   }
+}
+
+class PartInfo {
+  final String name;
+  final String description;
+  
+  PartInfo(this.name, {this.description: ""});
+  
+  bool operator==(other) {
+    if (other is! PartInfo) return false;
+    return other.name == this.name;
+  }
+  
+  int get hashCode => name.hashCode;
+  
+  String toString([int nameLength]) {
+    if (null == nameLength) nameLength = name.length;
+    return "  ${name.padRight(nameLength)}\t$description";
+  }
+  
 }
