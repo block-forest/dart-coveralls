b5647dbc6d0f826d7cb900afdb785194f7357d87
Kevin Moore <kevmoo@google.com>
2015-03-16 17:05:21 -0700

    tweak to bin/dart_coveralls.dart

--- bin/dart_coveralls.dart
+++ bin/dart_coveralls.dart
@@ -3,8 +3,7 @@ import "src/calc.dart";
 import "src/report.dart";
 
 void main(List<String> args) {
-  var builder = new CommandLineHubBuilder();
-  builder
+  var builder = new CommandLineHubBuilder()
     ..addPart("report", new ReportPart(),
         description: "Calculate and report coverage data to coveralls")
     ..addPart("calc", new CalcPart(),
@@ -12,5 +11,5 @@ void main(List<String> args) {
             "it in a file");
   var hub = builder.build();
 
-  return hub.parseAndExecute(args);
+  hub.parseAndExecute(args);
 }
