f11146238d77e13d63cffce03154918362ba40b9
Marko Locher <marko@locher.at>
2015-05-20 19:02:21 +0200

    Get the branch name out of the CI_BRANCH env variable if available.

--- lib/src/git_data.dart
+++ lib/src/git_data.dart
@@ -131,6 +131,8 @@ class GitBranch {
       {ProcessSystem processSystem: const ProcessSystem(),
       Map<String, String> environment}) {
     if (null == environment) environment = Platform.environment;
+    if (null != environment["CI_BRANCH"]) return environment[
+        "CI_BRANCH"];
     if (null != environment["TRAVIS_BRANCH"]) return environment[
         "TRAVIS_BRANCH"];
     var args = ["rev-parse", "--abbrev-ref", "HEAD"];
