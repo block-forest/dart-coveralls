d61764d3c5d44091af84e857a304513d61c2204e
Kevin Moore <kevmoo@google.com>
2015-05-24 12:56:07 -0700

    remove unused mock classes

--- test/mock_classes.dart
+++ test/mock_classes.dart
@@ -30,13 +30,3 @@ class ProcessResultMock extends Mock implements ProcessResult {
 class ProcessSystemMock extends Mock implements ProcessSystem {
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
-
-@proxy
-class SourceFilesReportsMock extends Mock implements SourceFileReports {
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
-}
-
-@proxy
-class GitDataMock extends Mock implements GitData {
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
-}
