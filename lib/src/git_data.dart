db46f82a3b90be894b782ae07fb5ad27098f6b9c
Kevin Moore <kevmoo@google.com>
2015-05-29 09:11:19 -0700

    git_data: more finals

--- lib/src/git_data.dart
+++ lib/src/git_data.dart
@@ -119,9 +119,9 @@ class GitRemote {
 }
 
 class GitBranch {
-  String name;
-  String reference;
-  String id;
+  final String name;
+  final String reference;
+  final String id;
 
   GitBranch(this.name, this.reference, this.id);
 
